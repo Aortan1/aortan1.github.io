867b6ab3271ad298c1dae0b1da5bd39c5d1124a8
Alex Dyomushkin <aortans@mail.com>
2016-06-25 13:52:47 +0300

    Creaitng viewport
    160625

---
+++ viewport/js/main.js
@@ -0,0 +1,52 @@
+
+function rand_till(n){
+ // если n = 6, ф-я выдаёт случайное из набора целых от 0 до 5.
+var r=Math.random();
+var rn = Math.floor(r*n);
+if (rn == n) {rn=0;}  
+return rn;  
+}
+
+$(document).ready(function (){
+  
+$('.slides').slick({
+  аccessibility: true,
+  dots: true,
+  infinite: true,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  speed: 1000,
+  draggable: true,
+  arrows: true,
+  fade: true,
+  cssEase: 'linear',
+  initialSlide: rand_till(15),
+  autoplay: true,
+  autoplaySpeed: 4000,
+  draggable: true 
+});
+
+  
+  $('.slides-control').slick({
+  slidesToShow: 5,
+  slidesToScroll: 1,
+  asNavFor: '.slides',
+  dots: false,
+  arrows: true,
+  centerMode: true,
+  focusOnSelect: true,
+  initialSlide: 08,
+  prevArrow: '.sl-prev',
+  nextArrow: '.sl-next',
+  
+  draggable: true
+});
+  
+  
+  
+  
+  
+  
+   
+}, 2000);
+
