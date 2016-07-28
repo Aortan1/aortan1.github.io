eae8bf025d47ba4113ed2137560dd9dffe7a5b3f
Alex Dyomushkin <aortans@mail.com>
2016-07-28 12:06:03 +0300

    sm23
    23

--- mastermind/js/main.js
+++ mastermind/js/main.js
@@ -10,7 +10,7 @@ var n_at=8; // число попыток или строк по 4 яцейки.
 
 var Itags = document.getElementsByTagName("i"); 
 var Btags = document.getElementsByTagName("b");
-if (n==4) {document.getElementsByClassName("ALL").classList.add("n-4-6");}
+// if (n==4) {document.getElementsByClassName("ALL").classList.add("n-4-6");}
 // if (n==4) {document.getElementsByTagName("body").classList.add("n-4-6");}
 // if (n==5) {
 //   document.querySelector("body").classList.add("n-5-8");
