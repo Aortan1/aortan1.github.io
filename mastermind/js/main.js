88a435ea0a76ccced9ee959593a3cc2b902f9c57
Alex Dyomushkin <aortans@mail.com>
2016-07-28 12:11:26 +0300

    sm25
    25

--- mastermind/js/main.js
+++ mastermind/js/main.js
@@ -1,6 +1,6 @@
 
 
-function (){
+
 
 var n = 4; // число ячеек в строке
 var n_colors = 6; // число вариантов цветов
@@ -11,6 +11,7 @@ var n_at=8; // число попыток или строк по 4 яцейки.
 
 var Itags = document.getElementsByTagName("i"); 
 var Btags = document.getElementsByTagName("b");
+function (){
  if (n==4) {document.getElementsByClassName("ALL").classList.add("n-4-6");}
 // if (n==4) {document.getElementsByTagName("body").classList.add("n-4-6");}
 // if (n==5) {
@@ -19,7 +20,9 @@ var Btags = document.getElementsByTagName("b");
 //   var Itags = document.querySelectorAll("i,tt");
 //   var Btags = document.querySelectorAll("b,em");
 //   } 
+}();
  // alert(Itags.length);
+}
 var Secrets = document.getElementsByClassName("secret"); 
 var Fon =  document.getElementsByClassName("fonc");
 var Htag = document.getElementsByTagName("h1");  
@@ -222,4 +225,4 @@ Btags[ii].style.borderColor = "black";
 }    
 
 }
-}
+
