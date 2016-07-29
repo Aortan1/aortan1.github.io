1f369cfff99a2519378bb397e0b94187d5d70712
Alex Dyomushkin <aortans@mail.com>
2016-07-29 14:35:46 +0300

    m65
    65

--- mastermind/js/main.js
+++ mastermind/js/main.js
@@ -208,8 +208,8 @@ Bcan =  document.getElementsByClassName("b_cansel");
  Bcan[0].style.fontSize = "15px";
  Bcan[0].style.fontWeight= "bold";   
            }   
-if (sum1==n && n==4){Htag[0].innerHTML="ВЫ РАЗГАДАЛИ 4 из 6 ЦВЕТОВ!!! ТЕПЕРЬ СУПЕРИГРА - 5 из 8.";}
 if (sum1==n && n==4){Htag[0].innerHTML="Отлично, комбинация разгадана!<br>А теперь СУПЕРИГРА:<br>5 цветов из 8.";}
+if (sum1==n && n==4){Htag[0].innerHTML="Отлично, вы угадали!<br>А теперь СУПЕРИГРА:<br>5 цветов из 8.";}
 if (sum1==n && n==5){Htag[0].innerHTML="ПОЗДРАВЛЯЮ!!! ВЫ РАЗГАДАЛИ КОМБИНАЦИЮ 5ти ЦВЕТОВ из 8!!!";}       
 if (sum1==n){
 // Htag[0].style.backgroundColor = "#388E3C"; Htag[0].style.color="white";
