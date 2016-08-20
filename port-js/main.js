37f1f4a63dadfb30c162ff596ac9e5cc0891f25d
Alex Dyomushkin <aortans@mail.com>
2016-08-20 21:53:23 +0300

    po65

--- port-js/main.js
+++ port-js/main.js
@@ -1,5 +1,33 @@
 
 
+
+
+// $(document).ready (function(){
+
+  
+   window.onscroll = function() {
+  
+  var scrolled = window.pageYOffset;
+  var winh = window.innerHeight; 
+  var h_light=00, h_ani=10, h_panel=670, h_to_top=1500;   
+  if (scrolled<h_ani)  $('.skills').removeClass('ani'); 
+  if (scrolled>h_ani) {
+    
+    $('.skills').removeClass('ani').addClass('ani');
+    
+    }
+ // if (scrolled>h_light && scrolled<h_light+200) $('.skill').removeClass('ho').addClass('ho');   
+ // if (scrolled<h_light || scrolled>h_light+200) $('.skill').removeClass('ho');
+ 
+ if (scrolled>h_panel) $('body').addClass('sticky'); 
+ if (scrolled<h_panel) $('body').removeClass('sticky');   
+ 
+ if (scrolled>h_to_top-winh) $('body').addClass('for-to-top');  
+ if (scrolled<h_to_top-winh) $('body').removeClass('for-to-top');     
+  };
+ // // });              
+ 
+
 $(document).ready (function(){ 
   
   $('a.contact-me').click(function ffade(){
@@ -12,34 +40,6 @@ $(document).ready (function(){
       $('section.me').fadeTo(1000,0.5).fadeTo(1000,1.0).fadeTo(1000,0.5).fadeTo(3000,1.0); 
    });
 });
-
-
-// $(document).ready (function(){
-
-  
- //   window.onscroll = function() {
-  
- //  var scrolled = window.pageYOffset;
- //  var winh = window.innerHeight; 
- //  var h_light=00, h_ani=10, h_panel=670, h_to_top=1500;   
- //  if (scrolled<h_ani)  $('.skills').removeClass('ani'); 
- //  if (scrolled>h_ani) {
-    
- //    $('.skills').removeClass('ani').addClass('ani');
-    
- //    }
- // // if (scrolled>h_light && scrolled<h_light+200) $('.skill').removeClass('ho').addClass('ho');   
- // // if (scrolled<h_light || scrolled>h_light+200) $('.skill').removeClass('ho');
- 
- // if (scrolled>h_panel) $('body').addClass('sticky'); 
- // if (scrolled<h_panel) $('body').removeClass('sticky');   
- 
- // if (scrolled>h_to_top-winh) $('body').addClass('for-to-top');  
- // if (scrolled<h_to_top-winh) $('body').removeClass('for-to-top');     
- //  };
- // // });              
- 
-
 
                
 
