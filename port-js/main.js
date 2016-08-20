4e37255b62566a297369caf3429c74164413baf0
Alex Dyomushkin <aortans@mail.com>
2016-08-20 20:35:48 +0300

    po40

--- port-js/main.js
+++ port-js/main.js
@@ -43,6 +43,6 @@
 $(function() {
     $('a.to-top').click(function(){
        $('html, body').animate({scrollTop:0}, 'slow');
-      $('section.me').fadeTo(1000,0).fadeTo(1000,1.0).fadeTo(1000,0.0).fadeTo(3000,1.0); 
+       $('section.me').fadeTo(1000,0).fadeTo(1000,1.0).fadeTo(1000,0.0).fadeTo(3000,1.0); 
    });
 });
