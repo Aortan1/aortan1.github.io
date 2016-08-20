20c254f32ace9655e142cc715b7ab3f2aba768d1
Alex Dyomushkin <aortans@mail.com>
2016-08-20 21:27:27 +0300

    po56

--- port-js/main.js
+++ port-js/main.js
@@ -5,38 +5,31 @@
 // $(document).ready (function(){
 
   
-   window.onscroll = function() {
+ //   window.onscroll = function() {
   
-  var scrolled = window.pageYOffset;
-  var winh = window.innerHeight; 
-  var h_light=00, h_ani=10, h_panel=670, h_to_top=1500;   
-  if (scrolled<h_ani)  $('.skills').removeClass('ani'); 
-  if (scrolled>h_ani) {
+ //  var scrolled = window.pageYOffset;
+ //  var winh = window.innerHeight; 
+ //  var h_light=00, h_ani=10, h_panel=670, h_to_top=1500;   
+ //  if (scrolled<h_ani)  $('.skills').removeClass('ani'); 
+ //  if (scrolled>h_ani) {
     
-    $('.skills').removeClass('ani').addClass('ani');
+ //    $('.skills').removeClass('ani').addClass('ani');
     
-    }
- // if (scrolled>h_light && scrolled<h_light+200) $('.skill').removeClass('ho').addClass('ho');   
- // if (scrolled<h_light || scrolled>h_light+200) $('.skill').removeClass('ho');
+ //    }
+ // // if (scrolled>h_light && scrolled<h_light+200) $('.skill').removeClass('ho').addClass('ho');   
+ // // if (scrolled<h_light || scrolled>h_light+200) $('.skill').removeClass('ho');
  
- if (scrolled>h_panel) $('body').addClass('sticky'); 
- if (scrolled<h_panel) $('body').removeClass('sticky');   
+ // if (scrolled>h_panel) $('body').addClass('sticky'); 
+ // if (scrolled<h_panel) $('body').removeClass('sticky');   
  
- if (scrolled>h_to_top-winh) $('body').addClass('for-to-top');  
- if (scrolled<h_to_top-winh) $('body').removeClass('for-to-top');     
-  };
- // });              
+ // if (scrolled>h_to_top-winh) $('body').addClass('for-to-top');  
+ // if (scrolled<h_to_top-winh) $('body').removeClass('for-to-top');     
+ //  };
+ // // });              
  
 
 
-// $(document).ready (function(){
-//  $('a.to-top').click(function () {
-//             $('html, body').animate({
-//                 scrollTop: 0
-//             }, 'slow');
-//             return false;
-//         });
-// });                
+               
 
 $(document).ready (function(){
     $('a.to-top').click(function(){
