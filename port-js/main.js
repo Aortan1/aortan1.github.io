789e4080593be3db1a2a786de5ff3da8e334feda
Alex Dyomushkin <aortans@mail.com>
2016-08-20 21:15:14 +0300

    po52

--- port-js/main.js
+++ port-js/main.js
@@ -38,15 +38,11 @@
 //         });
 // });                
 
-// $(function() {
-//     $('a.to-top').click(function(){
-//        $('html, body').animate({scrollTop:0}, 'slow');
-//    });
-// });
+$(function() {
+    $('a.to-top').click(function(){
+       $('html, body').animate({scrollTop:0}, 'slow');
+   });
+});
 
-function(){
-    $('a.to-top').click( function totop(){
-       $('body').animate({scrollTop: 0}, 4000);
-       $('section.me').fadeTo(1000,0).fadeTo(1000,1.0).fadeTo(1000,0.0).fadeTo(3000,1.0);
-       });
+
 
