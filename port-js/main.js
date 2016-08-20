6909830caf4db3f0b7b84856c7806eeb2cf15d19
Alex Dyomushkin <aortans@mail.com>
2016-08-20 21:50:29 +0300

    po63

--- port-js/main.js
+++ port-js/main.js
@@ -1,19 +1,18 @@
 
 
 $(document).ready (function(){ 
-	$('a.contact-me').click(function ffade(){
+  
+  $('a.contact-me').click(function ffade(){
 
-	$('section.me').fadeTo(1000,0.0).fadeTo(100000,1.0);
+	  $('img.img-me').fadeTo(1000,0.0).fadeTo(1000,0.1).fadeTo(1000,1.0);
 
 	});
-	
-    $('a.to-top').click(function ftop(){
-       alert("rrreeeddyyy!!!!!!");
-       $('html, body').animate({scrollTop: 300}, 4000);
-       $('section.me').fadeTo(1000,0.5).fadeTo(1000,1.0).fadeTo(1000,0.5).fadeTo(3000,1.0);
-       alert("rrreeeddyyy222222222!!!!!!");
+    $('a.to-top').click(function(){
+       $("body").animate({scrollTop: 0}, 'slow');
+      $('section.me').fadeTo(1000,0.5).fadeTo(1000,1.0).fadeTo(1000,0.5).fadeTo(3000,1.0); 
    });
 });
+
 
 // $(document).ready (function(){
 
