2ed48df62b629f6dd843230ffb21ecdfe8731110
Alex Dyomushkin <aortans@mail.com>
2016-08-22 10:23:48 +0300

    po78

--- port-js/main.js
+++ port-js/main.js
@@ -22,42 +22,19 @@ var difficulty_level=400; // уровень сложности (количест
 	});
 
 
-    $('a.to-top').click(function(){
+   //  $('a.to-top').click(function(){
 
-      $("html, body").animate({scrollTop: 0}, 3000);
-      $('section.me').fadeTo(1000,0.5).fadeTo(1000,1.0).fadeTo(1000,0.5).fadeTo(3000,1.0); 
+   //    $("html, body").animate({scrollTop: 0}, 3000);
+   //    $('section.me').fadeTo(1000,0.5).fadeTo(1000,1.0).fadeTo(1000,0.5).fadeTo(3000,1.0); 
    
-   });
-
+   // });
+$("a.to-top").click(function (){
+			$("body,html").animate({
+				scrollTop:0
+			}, 800);
+			return false;
+		});
  
   
  });
   
-//!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-
-
- 
-  
-  
-// Проверка ф-и вращения.
-//for (i=0; i<12; i++){
-//cu1 = rot(cu8,30*i,3); 
-//alert("a="+i*30+" koords: x="+cu1.x+" y="+cu1.y+" z="+cu1.z+" edge="+cu1.edge); 
-
-
-
-//var n1=Math.floor(Math.random()*3); alert(n1);
-  
-// alert (cube[1].y);  
-//for (i=0; i<54; i++){
-//document.write("<b> i="+i+" koords: x="+cube[i].x+" y="+cube[i].y+" z="+cube[i].z+" edge="+cube[i].edge+" COL="+cube[i].color+" nc="+ncell(cube[i])+"</b><br>"); 
-//}; 
-
-
-
-
-
-
-
-
-
