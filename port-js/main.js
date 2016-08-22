ce978a6c95aa24c4f28a400c1b6100db01a415e5
Alex Dyomushkin <aortans@mail.com>
2016-08-22 10:38:21 +0300

    po81

--- port-js/main.js
+++ port-js/main.js
@@ -1,33 +1,28 @@
 
 
-
+  window.onscroll = function() {
+  
+  var scrolled = window.pageYOffset;
+  var winh = window.innerHeight; 
+  var h_light=00, h_ani=10, h_panel=670, h_to_top=1500;   
+  if (scrolled<h_ani)  $('.skills').removeClass('ani'); 
+  if (scrolled>h_ani) {
+    
+    $('.skills').removeClass('ani').addClass('ani');
+    
+    }
+ // if (scrolled>h_light && scrolled<h_light+200) $('.skill').removeClass('ho').addClass('ho');   
+ // if (scrolled<h_light || scrolled>h_light+200) $('.skill').removeClass('ho');
+ 
+ if (scrolled>h_panel) $('body').addClass('sticky'); 
+ if (scrolled<h_panel) $('body').removeClass('sticky');   
+ 
+ if (scrolled>h_to_top-winh) $('body').addClass('for-to-top');  
+ if (scrolled<h_to_top-winh) $('body').removeClass('for-to-top');     
+  };
 
 $(document).ready (function(){ 
-var this1,this2;  
-var difficulty_level=400; // уровень сложности (количество движений размешивания)
-//3-5 - ЛЁГКИЙ,
-//6-9 - СРЕДНИЙ,
-//10 и > - СЛОЖНЫЙ.
 
-	 $('a.contact-me').hover(function fadeedge0(){
-  
-    $('h1').fadeTo(300,0.1); 
-  });
-  
-  
-  	$('a.contact-me').click(function ffade(){
-
-	  $('img.img-me').fadeTo(1000,0.0).fadeTo(1000,0.1).fadeTo(1000,1.0);
-
-	});
-
-
-   //  $('a.to-top').click(function(){
-
-   //    $("html, body").animate({scrollTop: 0}, 3000);
-   //    $('section.me').fadeTo(1000,0.5).fadeTo(1000,1.0).fadeTo(1000,0.5).fadeTo(3000,1.0); 
-   
-   // });
 $(function (){
 		// прячем кнопку #back-top
 		$("a.to-top").hide();
