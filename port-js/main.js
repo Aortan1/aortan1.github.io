ed9a1f1fb90faf0467562ab1a2e1788084f6fc5a
Alex Dyomushkin <aortans@mail.com>
2016-08-22 10:31:23 +0300

    po79

--- port-js/main.js
+++ port-js/main.js
@@ -28,13 +28,26 @@ var difficulty_level=400; // уровень сложности (количест
    //    $('section.me').fadeTo(1000,0.5).fadeTo(1000,1.0).fadeTo(1000,0.5).fadeTo(3000,1.0); 
    
    // });
-$("a.to-top").click(function (){
-			$("body,html").animate({
+$(function (){
+		// прячем кнопку #back-top
+		$("a.to-top").hide();
+	
+		$(window).scroll(function (){
+			if ($(this).scrollTop() > 100){
+				$("a.to-top").fadeIn();
+			} else{
+				$("a.to-top").fadeOut();
+			}
+		});
+
+		// при клике на ссылку плавно поднимаемся вверх
+		$("a.to-top").click(function (){
+			$("a.to-top").animate({
 				scrollTop:0
 			}, 800);
 			return false;
 		});
- 
+	});
   
  });
   
