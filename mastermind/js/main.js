6e16b01d36f7b556d9e52714584db65c47a8107f
Alex Dyomushkin <aortans@mail.com>
2016-09-20 10:16:28 +0300

    mast2000

--- mastermind/js/main.js
+++ mastermind/js/main.js
@@ -150,7 +150,8 @@ case false:
   window.location='http://aortan1.github.io/mastermind/?sup=5';
 }
   if (but.value=="NICE!" && n==5)  {
-  window.location='https://www.youtube.com/embed/tv3Rsz-EbzE?autoplay=1&start=9&?vq=480';
+  //window.location='https://www.youtube.com/embed/tv3Rsz-EbzE?autoplay=1&start=9&?vq=480';
+  window.location='https://www.youtube.com/embed/x2IkJ5RUxas?autoplay=1&start=0&?vq=480';
 }
 
 
