cefad17466179aa2a2c0d785b8326d8676578603
Alex Dyomushkin <aortans@mail.com>
2017-02-18 20:45:23 +0200

    we37

--- webcoder/js/main.js
+++ webcoder/js/main.js
@@ -10,11 +10,13 @@ function my_slider_slim (Element, delay, scrolltime){ // МОй СЛАЙДЕР. 
     var Ul = $(Element).find(".slides"); // элемент "список слайдов" .slides
     var Li = $(Element).find(".slides .sl"); // набор элементов "слайды" .sl
     var D = $(Element).find(".dots>.dot");  //набор элементов "дотсы" .dot
+    var B_left = $(Element).find(".left");  //левая кнопка перемотки
+    var B_right = $(Element).find(".right");  //правая кнопка перемотки
     var wi= $(Li).outerWidth(); //ширина слайда
     //var wi = 70;
     var n = $(Li).length; // количество слайдов
     var kbut = 1/5; //время прокрутки по кнопкам будет scrolltime * kbut
-    var id;
+    var id; // для id таймера задержки между слайдами
 
     function dota(i){  // выделение точки, соответствующей активному слайду по порядковому номеру
         //console.log("ii = "+i);
@@ -23,15 +25,33 @@ function my_slider_slim (Element, delay, scrolltime){ // МОй СЛАЙДЕР. 
         $(Element).find('.dot').eq(i).addClass('dot-active');
     }  
 
-    function scroll(i){ // функция единичной перемотки слайда и отмотки слайдов в начало при достижении конца
-      console.log("scroll-i = "+i);  
+    function scroll(i){ // функция единичной перемотки слайда и отмотки слайдов в начало при достижении конца 
       var nnow = $(Ul).data('nnow'); // извлечение номера текущего слайда.
-        if(i!=undefined) {$(Ul).animate({left: '-='+wi*(i-nnow)}, scrolltime*kbut).data('nnow', i);dota(i);return;}
-        nnow++; // переход к следующему слайду
-        if(nnow == n+1)  {nnow = 1; $(Ul).animate({left: '+='+wi*n}, 0);}  // сдвиг всех слайдов к началу, незаметный для глаза
-        $(Ul).animate({left: '-='+wi}, scrolltime).data('nnow', nnow); // анимация прокрутки слайда 
-        console.log("nnow = "+nnow+" left = "+$(Ul).position().left);
-        dota(nnow);
+      var scrollt = scrolltime;
+      var x_offset='-='+wi;
+      console.log("nnow0= "+nnow+" left0= "+$(Ul).position().left + " button-i = "+i);      
+      
+      if(i=="right") { 
+        x_offset='-='+wi;
+        scrollt = scrolltime*kbut;
+        nnow++;} else
+      if(i=="left") { 
+        x_offset='+='+wi;
+        scrollt = scrolltime*kbut;
+        nnow--;} else
+      if(i!=undefined) {
+        x_offset = '-='+wi*(i-nnow); 
+        scrollt = scrolltime*kbut;
+        nnow = i;}    
+      else nnow++; // переход к следующему слайду
+      
+      if(nnow == -1)  {nnow = n-1; $(Ul).animate({left: '-='+wi*n}, 0).data('nnow', nnow);}  // сдвиг всех слайдов к началу, незаметный для глаза     
+      $(Ul).animate({left: x_offset}, scrollt).data('nnow', nnow); // анимация прокрутки слайда 
+      if(nnow == n)  {nnow = 0; $(Ul).animate({left: '+='+wi*n}, 0).data('nnow', nnow);}  // сдвиг всех слайдов к началу, незаметный для глаза
+      dota(nnow);
+      
+      //console.log("nnow = "+nnow+" left = "+$(Ul).position().left);
+       
     } 
 
 
@@ -45,20 +65,21 @@ function my_slider_slim (Element, delay, scrolltime){ // МОй СЛАЙДЕР. 
 
 
 
-          $(D).click(
-          // function(){
-          //  clearInterval(id);
-          //  },      
+          $(D).click(     
           function(){
-            var ind = $(this).index();
-            //alert("click!!!");
-            console.log("ind = "+ind);
-            
-            scroll(ind);             
-            });           
-          // function(){
-          //    id = setInterval(scroll(), delay+scrolltime);
-          // });
+            var button_ind = $(this).index();
+            console.log("ind = "+button_ind);            
+            scroll(button_ind);
+            });
+      
+          $(B_left).click(      
+          function(){
+            scroll("left");
+            });   
+          $(B_right).click(     
+          function(){
+            scroll("right");
+            });
       
 
           $(Element).on('mouseenter', 
